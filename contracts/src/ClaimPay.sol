c3c1cd870c977bf0dca247a2fae87819b3725475
alcaniz <alcaniz.rst@gmail.com>
2026-09-08 09:37:47 +0200

    feat(contracts): préparer les transferts ERC20 sécurisés
    Importe l’interface IERC20 afin que ClaimPay puisse communiquer avec un token compatible ERC-20.
    
    Importe et associe SafeERC20 au type IERC20 pour sécuriser les futurs appels transfer et transferFrom.

--- contracts/src/ClaimPay.sol
+++ contracts/src/ClaimPay.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.35;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract ClaimPay {
+    using SafeERC20 for IERC20;
     error InvalidProvider();
     error InvalidArbiter();
     error EmptyMilestones();
